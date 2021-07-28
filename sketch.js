efec7e7d1ed998a154d4200fd4c9b8896f37cd3c
TanishqaK <88117370+TanishqaK@users.noreply.github.com>
2021-07-28 15:58:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background("white");
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x=box.position.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x=box.position.x-5
+  }
+drawSprites();
+}
+
+
+
+
